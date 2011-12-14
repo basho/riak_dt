3c64fab6e9559d49784a4647cb2621574b44eac7
Russell Brown <russell.brown@mac.com>
2011-12-14 15:14:04 +0000

    Refactored all storage calls out in internal API

--- apps/riak_crdt/src/riak_crdt_vnode.erl
+++ apps/riak_crdt/src/riak_crdt_vnode.erl
@@ -24,7 +24,9 @@
          merge/5,
          repair/4]).
 
--record(state, {partition, node, table}).
+-type key() :: {Mod :: atom(), Key :: term()}.
+
+-record(state, {partition, node, storage_state}).
 
 -define(MASTER, riak_crdt_vnode_master).
 -define(sync(PrefList, Command, Master),
@@ -52,50 +54,38 @@ repair(PrefList, Mod, Key, CRDT) ->
 %% Vnode API
 init([Partition]) ->
     Node = node(),
-    Table = "data/" ++ integer_to_list(Partition),
-    {ok, Table} = dets:open_file(Table, []),
-    {ok, #state { partition=Partition, node=Node, table=Table }}.
+    StorageState = start_storage(Partition),
+    {ok, #state { partition=Partition, node=Node, storage_state=StorageState }}.
 
-handle_command({value, Mod, Key, ReqId}, Sender, #state{partition=Idx, node=Node, table=Table}=State) ->
+handle_command({value, Mod, Key, ReqId}, Sender, #state{partition=Idx, node=Node, storage_state=StorageState}=State) ->
     lager:debug("value ~p ~p~n", [Mod, Key]),
-    Reply = case dets:lookup(Table, {Mod, Key}) of
-                [{{Mod, Key}, {Mod, Val}}] -> {Mod, Val};
-                [] -> notfound
-            end,
+    Reply = lookup({Mod, Key}, StorageState),
     riak_core_vnode:reply(Sender, {ReqId, {{Idx, Node}, Reply}}),
     {noreply, State};
-handle_command({update, Mod, Key, Args}, _Sender, #state{table=Table, partition=Idx}=State) ->
+handle_command({update, Mod, Key, Args}, _Sender, #state{storage_state=StorageState, partition=Idx}=State) ->
     lager:debug("update ~p ~p ~p~n", [Mod, Key, Args]),
-    Reply = case dets:lookup(Table, {Mod, Key}) of
-                [{{Mod, Key}, {Mod, Val}}] -> 
-                    Updated = Mod:update(Args, {node(), Idx}, Val),
-                    ok = dets:insert(Table, {{Mod, Key}, {Mod, Updated}}),
-                    {ok, {Mod, Updated}};
-                [] ->
-                    %% Not found, so create locally
-                    Updated = Mod:update(Args, {node(), Idx}, Mod:new()),
-                    ok = dets:insert(Table, {{Mod, Key}, {Mod, Updated}}),
-                    {ok, {Mod, Updated}}
-            end,
+    Current = case lookup({Mod, Key}, StorageState) of
+                  {Mod, Val} ->
+                      Val;
+                  notfound ->
+                      %% Not found, so create locally
+                      Mod:new()
+              end,
+    Updated = Mod:update(Args, {node(), Idx}, Current),
+    ok = store({{Mod, Key}, {Mod, Updated}}, StorageState),
+    Reply =   {ok, {Mod, Updated}},
     {reply, Reply, State};
-handle_command({merge, Mod, Key, {Mod, RemoteVal} = Remote, ReqId}, Sender, #state{table=Table}=State) ->
+handle_command({merge, Mod, Key, {Mod, RemoteVal}, ReqId}, Sender, #state{storage_state=StorageState}=State) ->
     lager:debug("Merge ~p ~p~n", [Mod, Key]),
-    Reply = case dets:lookup(Table, {Mod, Key}) of
-                [{{Mod, Key}, {Mod, LocalVal}}] ->
-                    Merged = Mod:merge(LocalVal, RemoteVal),
-                    dets:insert(Table, {{Mod, Key}, {Mod, Merged}});
-                [] ->
-                    dets:insert(Table, {{Mod, Key}, Remote})
-            end,
+    Reply = do_merge({Mod, Key}, RemoteVal, StorageState),
     riak_core_vnode:reply(Sender, {ReqId, Reply}),
     {noreply, State};
 handle_command(Message, _Sender, State) ->
     ?PRINT({unhandled_command, Message}),
     {noreply, State}.
 
-handle_handoff_command(?FOLD_REQ{foldfun=Fun, acc0=Acc0}, _Sender, State=#state{table=Table}) ->
-    F = fun({K, V}, AccIn) -> Fun(K, V, AccIn) end,
-    Acc = dets:foldl(F, Acc0, Table),
+handle_handoff_command(?FOLD_REQ{foldfun=Fun, acc0=Acc0}, _Sender, State=#state{storage_state=StorageState}) ->
+    Acc = fold(Fun, Acc0, StorageState),
     {reply, Acc, State}.
 
 handoff_starting(_TargetNode, State) ->
@@ -107,26 +97,17 @@ handoff_cancelled(State) ->
 handoff_finished(_TargetNode, State) ->
     {ok, State}.
 
-handle_handoff_data(Binary, #state{table=Table}=State) ->
+handle_handoff_data(Binary, #state{storage_state=StorageState}=State) ->
     {{Mod, Key}, {Mod, HoffVal}} = binary_to_term(Binary),
     %% merge with local
-     case dets:lookup(Table, {Mod, Key}) of
-         [{{Mod, Key}, {Mod, LocalVal}}] ->
-             Merged = Mod:merge(LocalVal, HoffVal),
-             dets:insert(Table, {{Mod, Key}, {Mod, Merged}});
-         [] ->
-             dets:insert(Table, {{Mod, Key}, {Mod, HoffVal}})
-         end,
+    ok = do_merge({Mod, Key}, HoffVal, StorageState),
     {reply, ok, State}.
 
 encode_handoff_item(Name, Value) ->
     term_to_binary({Name, Value}).
 
 is_empty(State) ->
-    case dets:info(State#state.table, no_keys) of
-        0 -> {true, State};
-        _ -> {false, State}
-    end.
+    {db_empty(State#state.storage_state), State}.
 
 delete(State) ->
     {ok, State}.
@@ -139,3 +120,51 @@ handle_exit(_Pid, _Reason, State) ->
 
 terminate(_Reason, _State) ->
     ok.
+
+%% priv
+do_merge({Mod, Key}, RemoteVal, StorageState) ->
+    Merged = case lookup({Mod, Key}, StorageState) of
+                 {Mod, LocalVal} ->
+                     Mod:merge(LocalVal, RemoteVal);
+                 notfound ->
+                     RemoteVal
+             end,
+    store({{Mod, Key}, {Mod, Merged}}, StorageState).
+
+%% Priv, storage stuff
+-spec start_storage(Partition :: integer()) ->
+                           StorageState :: term().
+start_storage(Partition) ->
+    Table = "data/" ++ integer_to_list(Partition),
+    {ok, Table} = dets:open_file(Table, []),
+    Table.
+
+-spec lookup(K :: key(), StorageState :: term()) ->
+                   {atom(), term()} | notfound.
+lookup(Key, StorageState) ->
+    Reply = case dets:lookup(StorageState, Key) of
+                [{{Mod, Key}, {Mod, Val}=Val}] ->
+                    Val;
+                [] ->
+                    notfound
+            end,
+    Reply.
+
+-spec store({K :: key(), {atom(), term()}}, StorageState :: term()) ->
+                   ok.
+store(KV, StorageState) ->
+    dets:insert(StorageState, KV).
+
+-spec fold(function(), list(), StorageState :: term()) ->
+                  list().
+fold(Fun, Acc0, StorageState) ->
+    F = fun({K, V}, AccIn) -> Fun(K, V, AccIn) end,
+    dets:foldl(F, Acc0, StorageState).
+
+-spec is_empty(StorageState :: term()) ->
+                      boolean().
+db_empty(StorageState) ->
+    case dets:info(StorageState, no_keys) of
+        0 -> true;
+        _ -> false
+    end.
