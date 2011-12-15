319d09f1da1ec96988661ff3213dd0b3a1ff818f
Russell Brown <russell.brown@mac.com>
2011-12-15 17:40:32 +0000

    Add incr/decr by amount to wm resource

--- apps/riak_crdt/src/riak_crdt_wm_pncounter.erl
+++ apps/riak_crdt/src/riak_crdt_wm_pncounter.erl
@@ -8,8 +8,8 @@
                 value :: term()}).
 add_routes() ->
     webmachine_router:add_route({["counters", key], ?MODULE, [{action, value}]}),
-    webmachine_router:add_route({["counters", key, "increment"], ?MODULE, [{action, increment}]}),
-    webmachine_router:add_route({["counters", key, "decrement"], ?MODULE, [{action, decrement}]}).
+    webmachine_router:add_route({["counters", key, "increment", '*'], ?MODULE, [{action, increment}]}),
+    webmachine_router:add_route({["counters", key, "decrement", '*'], ?MODULE, [{action, decrement}]}).
 
 init(Props) ->
     {ok, #state{
@@ -23,7 +23,7 @@ resource_exists(RD, #state{action=Action}=State) ->
             case riak_crdt_client:value(riak_crdt_pncounter, Key) of
                 Count when is_integer(Count) ->
                     {true, RD, State#state{value=Count, key=Key}};
-                {error, timeout} -> %% Currently times out if the counter doesn't exist
+                notfound ->
                     {false, RD, State#state{key=Key}};
                 Other ->
                     lager:info("Got error when fetching counter ~w: ~w~n",[Key, Other]),
@@ -45,14 +45,25 @@ content_types_provided(RD, State) ->
     {[{"text/plain", to_text}], RD, State}.
 
 process_post(RD, #state{action=Action, key=Key}=State) ->
-    case riak_crdt_client:update(riak_crdt_pncounter, Key, Action) of
+    UpdateOp = get_action(Action, wrq:disp_path(RD)),
+    case riak_crdt_client:update(riak_crdt_pncounter, Key, UpdateOp) of
         ok ->
             {true, RD, State};
         {error, timeout} ->
             {{halt, 503}, wrq:set_resp_body("Request timed out", RD), State};
+        {error, Reason} ->
+            {{halt, 500}, wrq:set_resp_body(Reason, RD), State};
         _Other ->
             {false, RD, State}
     end.
 
 to_text(RD, #state{value=Value}=State) ->
     {integer_to_list(Value), RD, State}.
+
+get_action(Action, Val)  ->
+    case (catch list_to_integer(Val)) of
+        I when is_integer(I), I > 0 ->
+            {Action, I};
+        _ -> Action
+    end.
+
