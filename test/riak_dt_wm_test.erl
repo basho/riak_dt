3d1544df4daf771e50befeb5d55011bf98008757
Sean Cribbs <sean@basho.com>
2012-10-12 07:08:01 -0700

    Fixed test failures in riak_dt_wm_test.
    We were missing a meck:unload() in the vnode test that caused regular
    vnode requests to fail in the WM test. Also, simplified the test
    format.

--- test/riak_dt_wm_test.erl
+++ test/riak_dt_wm_test.erl
@@ -13,40 +13,45 @@ setup(_) ->
     ok.
 
 pncounter_update_test_() ->
-    {setup,
-     fun() ->
-             riak_dt_test_util:setup("pncounter_update_test", fun setup/1)
-     end,
-     fun riak_dt_test_util:cleanup/1,
-     [
-      fun pncounter_increment/0,
-      fun pncounter_decrement/0,
-      fun pncounter_update_errors/0
-     ]}.
-
+     {setup,
+      fun() ->
+              Result = riak_dt_test_util:setup("pncounter_update_test", fun setup/1),
+              ok = riak_dt_test_util:wait_for_port(?HTTP_PORT, 5000),
+              Result
+      end,
+      fun riak_dt_test_util:cleanup/1,
+      {inparallel,
+       [
+             fun pncounter_increment/0,
+             fun pncounter_decrement/0,
+             fun pncounter_update_errors/0
+       ]}
+     }.
 
 pncounter_increment() ->
-    ?assertMatch({ok, {{_, 204, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pnincr", [], "text/plain", "1"}, [], [])),
-    ?assertMatch({ok, {{_, 200, _}, _, "1"}},
-                 httpc:request(?URL ++ "/counters/pnincr")),
-    ?assertMatch({ok, {{_, 204, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pnincr", [], "text/plain", "5"}, [], [])),
-    ?assertMatch({ok, {{_, 200, _}, _, "6"}},
-                 httpc:request(?URL ++ "/counters/pnincr")).
+    ?assertMatch({ok, {{_, 204, _}, _, _}}, request_counter("pnincr", "1")),
+    ?assertMatch({ok, {{_, 200, _}, _, "1"}}, request_counter("pnincr")),
+    ?assertMatch({ok, {{_, 204, _}, _, _}},  request_counter("pnincr", "5")),
+    ?assertMatch({ok, {{_, 200, _}, _, "6"}}, request_counter("pnincr")).
 
 pncounter_decrement() ->
-    ?assertMatch({ok, {{_, 204, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pndecr", [], "text/plain", "-1"}, [], [])),
-    ?assertMatch({ok, {{_, 200, _}, _, "-1"}},
-                 httpc:request(?URL ++ "/counters/pndecr")),
-    ?assertMatch({ok, {{_, 204, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pndecr", [], "text/plain", "-5"}, [], [])),
-    ?assertMatch({ok, {{_, 200, _}, _, "-6"}},
-                 httpc:request(?URL ++ "/counters/pndecr")).
+    ?assertMatch({ok, {{_, 204, _}, _, _}}, request_counter("pndecr", "-1")),
+    ?assertMatch({ok, {{_, 200, _}, _, "-1"}}, request_counter("pndecr")),
+    ?assertMatch({ok, {{_, 204, _}, _, _}}, request_counter("pndecr", "-5")),
+    ?assertMatch({ok, {{_, 200, _}, _, "-6"}}, request_counter("pndecr")).
 
 pncounter_update_errors() ->
-    ?assertMatch({ok, {{_, 400, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pnup", [], "text/plain", "-abc"}, [], [])),
-    ?assertMatch({ok, {{_, 400, _}, _, _}},
-                 httpc:request(post, {?URL ++ "/counters/pnup", [], "text/plain", ""}, [], [])).
+    ?assertMatch({ok, {{_, 400, _}, _, _}}, request_counter("pnup", "-abc")),
+    ?assertMatch({ok, {{_, 400, _}, _, _}}, request_counter("pnup", "")).
+
+request_counter(Key) ->
+    lager:debug("Fetching value of counter ~s~n", [Key]),
+    Result = httpc:request(?URL ++ "/counters/" ++ Key),
+    lager:debug("GET ~s returned ~p~n", [Key, Result]),
+    Result.
+
+request_counter(Key, Body) ->
+    lager:debug("Updating counter ~s with '~s'~n", [Key, Body]),
+    Result = httpc:request(post, {?URL ++ "/counters/" ++ Key , [], "text/plain", Body}, [], []),
+    lager:debug("POST ~s returned ~p~n", [Key, Result]),
+    Result.
