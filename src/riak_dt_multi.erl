0fe00f715b014835069e128914a00b87e589c3a1
Russell Brown <russell.brown@mac.com>
2013-06-08 18:23:51 +0100

    Add a document style holder for multiple CRDTS that is a CRDT

--- src/riak_dt_multi.erl
+++ src/riak_dt_multi.erl
@@ -40,64 +40,88 @@
 -export([gen_op/0, update_expected/3, eqc_state_value/1, init_state/0]).
 -endif.
 
+-opaque multi() :: {schema(), valuelist()}.
+-type schema() :: riak_dt_vvorset:vvorset().
+-type field() :: {Name::term(), Type::crdt_mod()}.
+-type crdt_mod() :: module(). %% @TODO add them all?
+-type valuelist() :: [{field(), Value::term()}]. %% @TODO add all the crdt types?
+
+-spec new() -> multi().
 new() ->
     {riak_dt_vvorset:new(), orddict:new()}.
 
-value({ADict, RDict}) ->
-    orddict:fetch_keys(orddict:filter(fun(K, AClock) ->
-                                        case orddict:find(K, RDict) of
-                                            {ok, RClock} ->
-                                                %% The element is not in the set
-                                                %% if the remove clock descends the add clock
-                                                %% In this case descends just means equals.
-                                                (not vclock:descends(RClock, AClock));
-                                            error -> true
-                                        end
-                                end,
-                                ADict)).
+value({Schema, Values}) ->
+    PresentFields = riak_dt_vvorset:value(Schema),
+    values(PresentFields, Values).
 
-update({add, Elem}, Actor, {ADict0, RDict}) ->
-    ADict = add_elem(Actor, ADict0, Elem),
-    {ADict, RDict};
-update({remove, Elem}, _Actor, {ADict, RDict0}) ->
-    RDict = remove_elem(orddict:find(Elem, ADict), Elem, RDict0),
-    {ADict, RDict}.
-merge({ADict1, RDict1}, {ADict2, RDict2}) ->
-    MergedADict = merge_dicts(ADict1, ADict2),
-    MergedRDict = merge_dicts(RDict1, RDict2),
-    {MergedADict, MergedRDict}.
+%% private
+values(PresentFields, Values) ->
+    values(PresentFields, Values, []).
 
-equal({ADict1, RDict1}, {ADict2, RDict2}) ->
-    ADict1 == ADict2 andalso RDict1 == RDict2.
+values([], _Values, Acc) ->
+    Acc;
+values([{_Name, Mod}=Key | Rest], Values, Acc) ->
+    CRDT = orddict:fetch(Key, Values),
+    Val = Mod:value(CRDT),
+    values(Rest, Values, [{Key, Val} | Acc]).
 
-%% Private
-add_elem(Actor, Dict, Elem) ->
-    VC = vclock:fresh(),
-    InitialValue = vclock:increment(Actor, VC),
-    orddict:update(Elem, update_fun(Actor), InitialValue, Dict).
+update({update, Ops}, Actor, {Schema, Values}) ->
+    apply_ops(Ops, Actor, Schema, Values).
 
-update_fun(Actor) ->
-    fun(Vclock) ->
-            vclock:increment(Actor, Vclock)
+apply_ops([], _Actor, Schema, Values) ->
+    {Schema, Values};
+apply_ops([{update, {_Name, Mod}=Key, Op} | Rest], Actor, Schema, Values) ->
+    %% Add the key to schema
+    NewSchema = riak_dt_vvorset:update({add, Key}, Actor, Schema),
+    %% Update the value
+    InitialValue0 = Mod:new(),
+    InitialValue = Mod:update(Op, Actor, InitialValue0),
+    NewValues = orddict:update(Key, update_fun(Mod, Op, Actor), InitialValue, Values),
+    apply_ops(Rest, Actor, NewSchema, NewValues);
+apply_ops([{remove, Key} | Rest], Actor, Schema, Values) ->
+    NewSchema = riak_dt_vvorset:update({remove, Key}, Actor, Schema),
+    apply_ops(Rest, Actor, NewSchema, Values);
+apply_ops([{add, {_Name, Mod}=Key} | Rest], Actor, Schema, Values) ->
+    %% Add the key to schema
+    NewSchema = riak_dt_vvorset:update({add, Key}, Actor, Schema),
+    %% Update the value, adding an empty crdt if not present
+    NewValues = orddict:update(Key, fun(V) -> V end, Mod:new(), Values),
+    apply_ops(Rest, Actor, NewSchema, NewValues).
+
+update_fun(Mod, Op, Actor) ->
+    fun(CRDT) ->
+            Mod:update(Op, Actor, CRDT)
     end.
 
-remove_elem({ok, Aclock}, Elem, RDict) ->
-    case orddict:find(Elem, RDict) of
-        {ok, Rclock} ->
-            orddict:store(Elem, vclock:merge([Aclock, Rclock]), RDict);
-        error ->
-            orddict:store(Elem, Aclock, RDict)
-    end;
-remove_elem(error, _Elem, RDict) ->
-    %% What @TODO?
-    %% Can't remove an element not in the ADict, warn??
-    %% Should we add to the remove set with a fresh + actor+1 vclock?
-    %% Throw an error? (seems best)
-    RDict.
+merge({Schema1, Values1}, {Schema2, Values2}) ->
+    NewSchema = riak_dt_vvorset:merge(Schema1, Schema2),
+    PresentFields = riak_dt_vvorset:value(NewSchema),
+    NewValues = merge_values(PresentFields, Values1, Values2, []),
+    %% This merges _all_ values in the value list.
+    %% Is that a good idea? It means merging items that maybe (long ago?!) deleted
+    %% However, maybe that is good in case they get re-added.
+    %% NewValues = orddict:merge(fun({_Name, Type}, V1, V2) -> Type:merge(V1, V2) end,
+    %%                           Values1, Values2),
+    {NewSchema, NewValues}.
 
-merge_dicts(Dict1, Dict2) ->
-    %% for every key in dict1, merge its contents with dict2's content for same key
-   orddict:merge(fun(_K, V1, V2) -> vclock:merge([V1, V2]) end, Dict1, Dict2).
+merge_values([], _Values1, _Values2, Acc) ->
+    orddict:from_list(Acc);
+merge_values([{_Name, Mod}=Key | Rest], Values1, Values2, Acc) ->
+    %% Safe fetch value from both dicts
+    %% Merge vaues
+    %% Add to acc under Key
+    V1 = type_safe_fetch(Mod, orddict:find(Key, Values1)),
+    V2 = type_safe_fetch(Mod, orddict:find(Key, Values2)),
+    V = Mod:merge(V1, V2),
+    merge_values(Rest, Values1, Values2, [{Key, V} | Acc]).
+
+type_safe_fetch(_Mod, {ok, Value}) ->
+    Value;
+type_safe_fetch(Mod, error) ->
+    Mod:new().
+
+equal({Schema1, Values1}, {Schema2, Values2}) ->
+    riak_dt_vvorset:equal(Schema1, Schema2)  andalso Values1 == Values2.
 
 %% ===================================================================
 %% EUnit tests
@@ -105,46 +129,46 @@ merge_dicts(Dict1, Dict2) ->
 -ifdef(TEST).
 
 -ifdef(EQC).
-eqc_value_test_() ->
-    {timeout, 120, [?_assert(crdt_statem_eqc:prop_converge(init_state(), 1000, ?MODULE))]}.
+%% eqc_value_test_() ->
+%%     {timeout, 120, [?_assert(crdt_statem_eqc:prop_converge(init_state(), 1000, ?MODULE))]}.
 
-%% EQC generator
-gen_op() ->
-    ?LET({Add, Remove}, gen_elems(),
-         oneof([{add, Add}, {remove, Remove}])).
+%% %% EQC generator
+%% gen_op() ->
+%%     ?LET({Add, Remove}, gen_elems(),
+%%          oneof([{add, Add}, {remove, Remove}])).
 
-gen_elems() ->
-    ?LET(A, int(), {A, oneof([A, int()])}).
+%% gen_elems() ->
+%%     ?LET(A, int(), {A, oneof([A, int()])}).
 
-init_state() ->
-    {0, dict:new(), []}.
+%% init_state() ->
+%%     {0, dict:new(), []}.
 
-update_expected(ID, {add, Elem}, {Cnt0, Dict, L}) ->
-    Cnt = Cnt0+1,
-    ToAdd = {Elem, Cnt},
-    {A, R} = dict:fetch(ID, Dict),
-    {Cnt, dict:store(ID, {sets:add_element(ToAdd, A), R}, Dict), [{ID, {add, Elem}}|L]};
-update_expected(ID, {remove, Elem}, {Cnt, Dict, L}) ->
-    {A, R} = dict:fetch(ID, Dict),
-    ToRem = [ {E, X} || {E, X} <- sets:to_list(A), E == Elem],
-    {Cnt, dict:store(ID, {A, sets:union(R, sets:from_list(ToRem))}, Dict), [{ID, {remove, Elem, ToRem}}|L]};
-update_expected(ID, {merge, SourceID}, {Cnt, Dict, L}) ->
-    {FA, FR} = dict:fetch(ID, Dict),
-    {TA, TR} = dict:fetch(SourceID, Dict),
-    MA = sets:union(FA, TA),
-    MR = sets:union(FR, TR),
-    {Cnt, dict:store(ID, {MA, MR}, Dict), [{ID,{merge, SourceID}}|L]};
-update_expected(ID, create, {Cnt, Dict, L}) ->
-    {Cnt, dict:store(ID, {sets:new(), sets:new()}, Dict), [{ID, create}|L]}.
+%% update_expected(ID, {add, Elem}, {Cnt0, Dict, L}) ->
+%%     Cnt = Cnt0+1,
+%%     ToAdd = {Elem, Cnt},
+%%     {A, R} = dict:fetch(ID, Dict),
+%%     {Cnt, dict:store(ID, {sets:add_element(ToAdd, A), R}, Dict), [{ID, {add, Elem}}|L]};
+%% update_expected(ID, {remove, Elem}, {Cnt, Dict, L}) ->
+%%     {A, R} = dict:fetch(ID, Dict),
+%%     ToRem = [ {E, X} || {E, X} <- sets:to_list(A), E == Elem],
+%%     {Cnt, dict:store(ID, {A, sets:union(R, sets:from_list(ToRem))}, Dict), [{ID, {remove, Elem, ToRem}}|L]};
+%% update_expected(ID, {merge, SourceID}, {Cnt, Dict, L}) ->
+%%     {FA, FR} = dict:fetch(ID, Dict),
+%%     {TA, TR} = dict:fetch(SourceID, Dict),
+%%     MA = sets:union(FA, TA),
+%%     MR = sets:union(FR, TR),
+%%     {Cnt, dict:store(ID, {MA, MR}, Dict), [{ID,{merge, SourceID}}|L]};
+%% update_expected(ID, create, {Cnt, Dict, L}) ->
+%%     {Cnt, dict:store(ID, {sets:new(), sets:new()}, Dict), [{ID, create}|L]}.
 
-eqc_state_value({_Cnt, Dict, _L}) ->
-    {A, R} = dict:fold(fun(_K, {Add, Rem}, {AAcc, RAcc}) ->
-                               {sets:union(Add, AAcc), sets:union(Rem, RAcc)} end,
-                       {sets:new(), sets:new()},
-                       Dict),
-    Remaining = sets:subtract(A, R),
-    Values = [ Elem || {Elem, _X} <- sets:to_list(Remaining)],
-    lists:usort(Values).
+%% eqc_state_value({_Cnt, Dict, _L}) ->
+%%     {A, R} = dict:fold(fun(_K, {Add, Rem}, {AAcc, RAcc}) ->
+%%                                {sets:union(Add, AAcc), sets:union(Rem, RAcc)} end,
+%%                        {sets:new(), sets:new()},
+%%                        Dict),
+%%     Remaining = sets:subtract(A, R),
+%%     Values = [ Elem || {Elem, _X} <- sets:to_list(Remaining)],
+%%     lists:usort(Values).
 
 -endif.
 -endif.
