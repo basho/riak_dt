475db29c52618811baff25ab1f8efb3d70de9044
Russell Brown <russell.brown@mac.com>
2014-02-20 18:12:15 +0000

    Remove riak_kv copypasta, largify the number of replicas

--- test/orswot_eqc.erl
+++ test/orswot_eqc.erl
@@ -29,19 +29,15 @@
 
 -compile(export_all).
 
--record(state,{vnodes=[] :: [binary()], %% Sort of like the ring, upto N*2 vnodeids
-               vnode_data=[] :: [{VNodeID :: binary(),
+-record(state,{replicas=[] :: [binary()], %% Sort of like the ring, upto N*2 ids
+               replica_data=[] :: [{ActorId :: binary(),
                                   riak_dt_orswot:orswot(),
                                   riak_dt_orset:orset()}],
-               %% The data, duplicated values for vnodes
+               %% The data, duplicated values for replicas
                %% Newest at the head of the list.
                %% Prepend only data 'cos symbolic / dynamic state.
-               last_get=undefined :: undefined | {undefined | riak_dt_orswot:orswot(), undefined | riak_dt_orset:orset()},
-               %% Stash the last `get' result so we can use it to put later
-               n=0 :: integer(), %% Generated NVal
-               r=0 :: integer(), %% Generated R quorum
-               time=0 :: integer(),  %% For the vclocks
-               adds=[] :: [{VNodeID :: binary(), atom()}] %% things that have been added
+               n=0 :: integer(), %% Generated number of replicas
+               adds=[] :: [{ActorId :: binary(), atom()}] %% things that have been added
               }).
 
 -define(NUMTESTS, 1000).
@@ -68,60 +64,61 @@ initial_state() ->
 
 
 %% ------ Grouped operator: set_nr
-%% Only set N, R if N has not been set (ie run once, as the first command)
+%% Only set N if N has not been set (ie run once, as the first command)
 set_nr_pre(#state{n=N}) ->
      N == 0.
 
 set_nr_args(_S) ->
-    [?LET(NVal, choose(2, 4), {NVal, choose(1, NVal)})].
+    [choose(3, 10)].
 
 set_nr(_) ->
     %% Command args used for next state only
     ok.
 
-set_nr_next(S, _V, [{N, R}]) ->
-    S#state{n=N, r=R}.
+set_nr_next(S, _V, [N]) ->
+    S#state{n=N}.
 
 %% ------ Grouped operator: make_ring
-%% Generate a bunch of vnodes, only runs until enough are generated
-make_ring_pre(#state{vnodes=VNodes, n=N}) ->
-    N > 0 andalso length(VNodes) < N * 2.
+%% Generate a bunch of replicas,
+%% only runs until enough are generated
+make_ring_pre(#state{replicas=Replicas, n=N}) ->
+    N > 0 andalso length(Replicas) < N * 2.
 
-make_ring_args(#state{vnodes=VNodes, n=N}) ->
-    [VNodes, vector(N, binary(8))].
+make_ring_args(#state{replicas=Replicas, n=N}) ->
+    [Replicas, vector(N, binary(8))].
 
 make_ring(_,_) ->
     %% Command args used for next state only
     ok.
 
-make_ring_next(S=#state{vnodes=VNodes}, _V, [_, NewVNodes0]) ->
-    %% No duplicate vnode ids please!
-    NewVNodes = lists:filter(fun(Id) -> not lists:member(Id, VNodes) end, NewVNodes0),
-    S#state{vnodes=VNodes ++ NewVNodes}.
+make_ring_next(S=#state{replicas=Replicas}, _V, [_, NewReplicas0]) ->
+    %% No duplicate replica ids please!
+    NewReplicas = lists:filter(fun(Id) -> not lists:member(Id, Replicas) end, NewReplicas0),
+    S#state{replicas=Replicas ++ NewReplicas}.
 
 %% ------ Grouped operator: put
 %% Store a new value, a contextless put
 add_pre(S) ->
-    vnodes_ready(S).
+    replicas_ready(S).
 
-add_args(#state{vnodes=VNodes, vnode_data=VNodeData}) ->
+add_args(#state{replicas=Replicas, replica_data=ReplicaData}) ->
     [
      oneof(['X', 'Y', 'Z']), %% a new value
-     elements(VNodes), % The replica
-     VNodeData %% The existing vnode data
+     elements(Replicas), % The replica
+     ReplicaData %% The existing vnode data
     ].
 
 %% Add a value to the set
-add(Value, Actor, VNodeData) ->
-    {ORSWOT, ORSet} = get(Actor, VNodeData),
+add(Value, Actor, ReplicaData) ->
+    {ORSWOT, ORSet} = get(Actor, ReplicaData),
     {ok, ORSWOT2} = riak_dt_orswot:update({add, Value}, Actor, ORSWOT),
     {ok, ORSet2} = riak_dt_orset:update({add, Value}, Actor, ORSet),
     {Actor, ORSWOT2, ORSet2}.
 
-add_next(S=#state{vnode_data=VNodeData, adds=Adds}, Res, [Value, Coord, _]) ->
+add_next(S=#state{replica_data=ReplicaData, adds=Adds}, Res, [Value, Coord, _]) ->
     %% The state data is prepend only, it grows and grows, but it's based on older state
     %% Newest at the front.
-    S#state{vnode_data=[Res | VNodeData], adds=[{Coord, Value} | Adds]}.
+    S#state{replica_data=[Res | ReplicaData], adds=[{Coord, Value} | Adds]}.
 
 add_post(_S, _Args, Res) ->
     post_all(Res, add).
@@ -130,26 +127,26 @@ add_post(_S, _Args, Res) ->
 %% remove, but only something that has been added already
 remove_pre(S=#state{adds=Adds}) ->
     %% Only do an update if you already did a get
-    vnodes_ready(S) andalso Adds /= [].
+    replicas_ready(S) andalso Adds /= [].
 
-remove_args(#state{adds=Adds, vnode_data=VNodeData}) ->
+remove_args(#state{adds=Adds, replica_data=ReplicaData}) ->
     [
      elements(Adds), %% Something that has been added
-     VNodeData %% All the vnode data
+     ReplicaData %% All the vnode data
     ].
 
 remove_pre(#state{adds=Adds}, [Add, _]) ->
     lists:member(Add, Adds).
 
-remove({VNode, Value}, VNodeData) ->
-    {ORSWOT, ORSet} = get(VNode, VNodeData),
+remove({Replica, Value}, ReplicaData) ->
+    {ORSWOT, ORSet} = get(Replica, ReplicaData),
     %% even though we only remove what has been added, there is no
     %% guarantee a merge from another replica hasn't led to the
-    %% element being absent already, so ignore precon errors (they
+    %% element being removed already, so ignore precon errors (they
     %% don't change state)
-    ORSWOT2 = ignore_preconerror_remove(Value, VNodeData, ORSWOT, riak_dt_orswot),
-    ORSet2 = ignore_preconerror_remove(Value, VNodeData, ORSet, riak_dt_orset),
-    {VNode, ORSWOT2, ORSet2}.
+    ORSWOT2 = ignore_preconerror_remove(Value, ReplicaData, ORSWOT, riak_dt_orswot),
+    ORSet2 = ignore_preconerror_remove(Value, ReplicaData, ORSet, riak_dt_orset),
+    {Replica, ORSWOT2, ORSet2}.
 
 ignore_preconerror_remove(Value, Actor, Set, Mod) ->
     case Mod:update({remove, Value}, Actor, Set) of
@@ -159,45 +156,46 @@ ignore_preconerror_remove(Value, Actor, Set, Mod) ->
             Set
     end.
 
-remove_next(S=#state{vnode_data=VNodeData, adds=Adds}, Res, [Add, _]) ->
+remove_next(S=#state{replica_data=ReplicaData, adds=Adds}, Res, [Add, _]) ->
     %% The state data is prepend only, it grows and grows, but it's based on older state
     %% Newest at the front.
-    S#state{vnode_data=[Res | VNodeData], adds=lists:delete(Add, Adds)}.
+    S#state{replica_data=[Res | ReplicaData], adds=lists:delete(Add, Adds)}.
 
 remove_post(_S, _Args, Res) ->
     post_all(Res, remove).
 
 %% ------ Grouped operator: replicate
-%% Merge two replicas values
-replicate_pre(S=#state{vnode_data=VNodeData}) ->
-    vnodes_ready(S) andalso VNodeData /= [].
+%% Merge two replicas' values
+replicate_pre(S=#state{replica_data=ReplicaData}) ->
+    replicas_ready(S) andalso ReplicaData /= [].
 
-replicate_args(#state{vnodes=VNodes, vnode_data=VNodeData}) ->
+replicate_args(#state{replicas=Replicas, replica_data=ReplicaData}) ->
     [
-     elements(VNodes), %% Replicate from
-     elements(VNodes), %% replicate too
-     VNodeData
+     elements(Replicas), %% Replicate from
+     elements(Replicas), %% replicate too
+     ReplicaData
     ].
 
+%% Don't replicate to oneself
 replicate_pre(_S, [VN, VN, _]) ->
     false;
 replicate_pre(_S, [_VN1, _VN2, _]) ->
     true.
 
-%% Mutating multiple elements in vnode_data in place is bad idea
-%% (symbolic vs dynamic state), so instead of treating Put and
+%% Mutating multiple elements in replica_data in place is bad idea
+%% (symbolic vs dynamic state), so instead of treating add/remove and
 %% replicate as the same action, this command handles the replicate
-%% part of the put fsm. Data from some random vnode (From) is
-%% replicated to some random vnode (To)
-replicate(From, To, VNodeData) ->
-    {FromORSWOT, FromORSet} = get(From, VNodeData),
-    {ToORSWOT, ToORSet} = get(To, VNodeData),
+%% part of the. Data from some random replica (From) is replicated to
+%% some random replica (To)
+replicate(From, To, ReplicaData) ->
+    {FromORSWOT, FromORSet} = get(From, ReplicaData),
+    {ToORSWOT, ToORSet} = get(To, ReplicaData),
     ORSWOT = riak_dt_orswot:merge(FromORSWOT, ToORSWOT),
     ORSet = riak_dt_orset:merge(FromORSet, ToORSet),
     {To, ORSWOT, ORSet}.
 
-replicate_next(S=#state{vnode_data=VNodeData}, Res, _Args) ->
-    S#state{vnode_data=[Res | VNodeData]}.
+replicate_next(S=#state{replica_data=ReplicaData}, Res, _Args) ->
+    S#state{replica_data=[Res | ReplicaData]}.
 
 replicate_post(_S, _Args, Res) ->
     post_all(Res, rep).
@@ -206,19 +204,19 @@ replicate_post(_S, _Args, Res) ->
 prop_merge() ->
     ?FORALL(Cmds, commands(?MODULE),
             begin
-                {H, S=#state{vnodes=VNodes, vnode_data=VNodeData}, Res} = run_commands(?MODULE,Cmds),
-                %% Check that collapsing all values leads to the same results for dvv and riak_object
-                {OV, ORV} = case VNodes of
+                {H, S=#state{replicas=Replicas, replica_data=ReplicaData}, Res} = run_commands(?MODULE,Cmds),
+                %% Check that collapsing all values leads to the same results for ORSWOT and ORSet
+                {OV, ORV} = case Replicas of
                                 [] ->
                                     {[], []};
                                 _L ->
                                     %% Get ALL actor's values
                                     {OS, OR} = lists:foldl(fun(Actor, {O, OR}) ->
-                                                                   {O1, OR1} = get(Actor, VNodeData),
+                                                                   {O1, OR1} = get(Actor, ReplicaData),
                                                                    {riak_dt_orswot:merge(O, O1),
                                                                     riak_dt_orset:merge(OR, OR1)} end,
                                                            {riak_dt_orswot:new(), riak_dt_orset:new()},
-                                                           VNodes),
+                                                           Replicas),
                                     {riak_dt_orswot:value(OS), riak_dt_orset:value(OR)}
                             end,
                         aggregate(command_names(Cmds),
@@ -231,8 +229,8 @@ prop_merge() ->
 %% -----------
 %% Helpers
 %% ----------
-vnodes_ready(#state{vnodes=VNodes, n=N}) ->
-    length(VNodes) >= N andalso N > 0.
+replicas_ready(#state{replicas=Replicas, n=N}) ->
+    length(Replicas) >= N andalso N > 0.
 
 post_all({_, ORSWOT, ORSet}, Cmd) ->
     %% What matters is that both types have the exact same results.
@@ -240,15 +238,15 @@ post_all({_, ORSWOT, ORSet}, Cmd) ->
         true ->
             true;
         _ ->
-            {postcondition_failed, "Swot and Set don't match", Cmd, ORSWOT, ORSet}
+            {postcondition_failed, "SWOT and Set don't match", Cmd, ORSWOT, ORSet}
     end.
 
 
-%% if a vnode does not yet have vnode data, return `undefined` for the
-%% riak_object and DVVSet.
-get(VNode, VNodeData) ->
-    case lists:keyfind(VNode, 1, VNodeData) of
-        {VNode, ORSWOT, ORSet} ->
+%% if a replica does not yet have replica data, return `new()` for the
+%% ORSWOT and ORSet
+get(Replica, ReplicaData) ->
+    case lists:keyfind(Replica, 1, ReplicaData) of
+        {Replica, ORSWOT, ORSet} ->
             {ORSWOT, ORSet};
         false -> {riak_dt_orswot:new(), riak_dt_orset:new()}
     end.
