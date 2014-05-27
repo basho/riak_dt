243dc62ff6511275cbc82245b53223c5b8ce233a
Russell Brown <russell.brown@mac.com>
2014-05-27 17:43:02 +0100

    Re-write od_flag_eqc to shrink correctly

--- test/od_flag_eqc.erl
+++ test/od_flag_eqc.erl
@@ -1,8 +1,8 @@
 %% -------------------------------------------------------------------
 %%
-%% map_eqc: Drive out the merge bugs the other statem couldn't
+%% od_flag_eqc: Drive out the merge bugs the other statem couldn't
 %%
-%% Copyright (c) 2007-2012 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2007-2014 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -28,20 +28,7 @@
 
 -compile(export_all).
 
--type model() :: {Cntr :: pos_integer(), %% Unique ID per operation
-                      Enables :: set(), %% enables
-                      Disable :: set(), %% Tombstones
-                      %% Disable that are waiting for enables before they
-                      %% can be run (like context ops in the
-                      %% riak_dt_od_flag)
-                      Deferred :: set()
-                     }.
-
 -record(state,{replicas=[] :: [binary()], %% Sort of like the ring, upto N*2 ids
-               replica_data=[] :: [{ActorId :: binary(),
-                                    riak_dt_od_flag:od_flag(),
-                                    model()}],
-               n=0 :: pos_integer(), %% Generated number of replicas
                counter=1 :: pos_integer() %% a unique tag per add
               }).
 
@@ -67,187 +54,199 @@ check() ->
 initial_state() ->
     #state{}.
 
-%% ------ Grouped operator: set_nr
-%% Only set N if N has not been set (ie run once, as the first command)
-set_n_pre(#state{n=N}) ->
-     N == 0.
+%% ------ Grouped operator: create_replica
+create_replica_pre(#state{replicas=Replicas}) ->
+    length(Replicas) < 10.
 
-%% Choose how many replicas to have in the system
-set_n_args(_S) ->
-    [choose(2, 10)].
+%% @doc create_replica_command - Command generator
+create_replica_args(_S) ->
+    %% Don't waste time shrinking the replicas ID binaries, they 8
+    %% byte binaris as that is riak-esque.
+    [noshrink(binary(8))].
 
-set_n(_) ->
-    %% Command args used for next state only
-    ok.
+%% @doc create_replica_pre - don't create a replica that already
+%% exists
+-spec create_replica_pre(S :: eqc_statem:symbolic_state(),
+                         Args :: [term()]) -> boolean().
+create_replica_pre(#state{replicas=Replicas}, [Id]) ->
+    not lists:member(Id, Replicas).
 
-set_n_next(S, _V, [N]) ->
-    S#state{n=N}.
+%% @doc create a new replica, and store a bottom orswot/orset+deferred
+%% in ets
+create_replica(Id) ->
+    ets:insert(od_flag_eqc, {Id, riak_dt_od_flag:new(), model_new()}).
 
-%% ------ Grouped operator: make_ring
-%%
-%% Generate a bunch of replicas, only runs if N is set, and until
-%% "enough" are generated (N*2) is more than enough.
-make_ring_pre(#state{replicas=Replicas, n=N}) ->
-    N > 0 andalso length(Replicas) < N * 2.
-
-make_ring_args(#state{replicas=Replicas, n=N}) ->
-    [Replicas, vector(N, binary(8))].
-
-make_ring(_,_) ->
-    %% Command args used for next state only
-    ok.
-
-make_ring_next(S, _V, [Replicas0, NewReplicas]) ->
-    %% No duplicate replica ids please!
-    Replicas = lists:umerge(Replicas0, NewReplicas),
-    S#state{replicas=Replicas}.
+%% @doc create_replica_next - Add the new replica ID to state
+-spec create_replica_next(S :: eqc_statem:symbolic_state(),
+                          V :: eqc_statem:var(),
+                          Args :: [term()]) -> eqc_statem:symbolic_state().
+create_replica_next(S=#state{replicas=R0}, _Value, [Id]) ->
+    S#state{replicas=R0++[Id]}.
 
 %% ------ Grouped operator: enable
 %% enable the flag
-enable_pre(S) ->
-    replicas_ready(S).
+enable_pre(#state{replicas=Replicas}) ->
+    Replicas /= [].
 
-enable_args(#state{replicas=Replicas, replica_data=ReplicaData, counter=Cnt}) ->
+enable_args(#state{replicas=Replicas, counter=Cnt}) ->
     [
      elements(Replicas), % The replica
-     Cnt,
-     ReplicaData %% The existing vnode data
+     Cnt
     ].
 
+enable_pre(#state{replicas=Replicas}, [Replica, _Cnt]) ->
+    lists:member(Replica, Replicas).
 
-enable(Actor, Cnt, ReplicaData) ->
-    {Flag, Model} = get(Actor, ReplicaData),
+enable(Actor, Cnt) ->
+    [{Actor, Flag, Model}] = ets:lookup(od_flag_eqc, Actor),
     {ok, Flag2} = riak_dt_od_flag:update(enable, Actor, Flag),
     {ok, Model2} = model_enable(Cnt, Model),
-    {Actor, Flag2, Model2}.
+    ets:insert(od_flag_eqc, {Actor, Flag2, Model2}),
+    {Flag2, Model2}.
 
-enable_next(S=#state{replica_data=ReplicaData, counter=Cnt}, Res, [_, _, _]) ->
-    S#state{replica_data=[Res | ReplicaData], counter=Cnt+1}.
+enable_next(S=#state{counter=Cnt}, _Res, [_, _]) ->
+    S#state{counter=Cnt+1}.
 
 enable_post(_S, _Args, Res) ->
     post_all(Res, enable).
 
 %% ------ Grouped operator: disable
 
-disable_pre(S=#state{}) ->
-    replicas_ready(S).
+disable_pre(#state{replicas=Replicas}) ->
+    Replicas /= [].
 
-disable_args(#state{replicas=Replicas, replica_data=ReplicaData}) ->
+disable_args(#state{replicas=Replicas}) ->
     [
-     elements(Replicas),
-     ReplicaData %% All the vnode data
+     elements(Replicas)
     ].
 
-disable(Replica, ReplicaData) ->
-    {Flag, Model} = get(Replica, ReplicaData),
+disable_pre(#state{replicas=Replicas}, [Replica]) ->
+    lists:member(Replica, Replicas).
+
+disable(Replica) ->
+    [{Replica, Flag, Model}] = ets:lookup(od_flag_eqc, Replica),
     {ok, Flag2} = riak_dt_od_flag:update(disable, Replica, Flag),
     Model2 = model_disable(Model),
-    {Replica, Flag2, Model2}.
-
-disable_next(S=#state{replica_data=ReplicaData}, Res, [_, _]) ->
-    S#state{replica_data=[Res | ReplicaData]}.
+    ets:insert(od_flag_eqc, {Replica, Flag2, Model2}),
+    {Flag2, Model2}.
 
 disable_post(_S, _Args, Res) ->
     post_all(Res, disable).
 
 %% ------ Grouped operator: ctx_disable
 %% disable, but with a context
-ctx_disable_pre(S=#state{replica_data=ReplicaData}) ->
-    replicas_ready(S) andalso ReplicaData /= [].
+ctx_disable_pre(#state{replicas=Replicas}) ->
+    Replicas /= [].
 
-ctx_disable_args(#state{replicas=Replicas, replica_data=ReplicaData}) ->
+ctx_disable_args(#state{replicas=Replicas}) ->
     [
      elements(Replicas), %% read from
-     elements(Replicas), %% send op too
-     ReplicaData %% All the vnode data
+     elements(Replicas) %% send op too
     ].
 
-%% Should we send ctx ops to originating replica?
-ctx_disable_pre(_S, [VN, VN, _]) ->
-    false;
-ctx_disable_pre(_S, [_VN1, _VN2, _]) ->
-    true.
+%% @doc ctx_disable_pre - Ensure correct shrinking
+-spec ctx_disable_pre(S :: eqc_statem:symbolic_state(),
+                         Args :: [term()]) -> boolean().
+ctx_disable_pre(#state{replicas=Replicas}, [From, To]) ->
+    lists:member(From, Replicas) andalso lists:member(To, Replicas).
 
-ctx_disable(From, To, ReplicaData) ->
-    {FromFlag, FromModel} = get(From, ReplicaData),
-    {ToFlag, ToModel} = get(To, ReplicaData),
+%% @doc a dynamic precondition uses concrete state, check that the
+%% `From' flag is enabled
+ctx_disable_dynamicpre(_S, [From, _To]) ->
+    [{From, Flag, _FromORSet}] = ets:lookup(od_flag_eqc, From),
+    riak_dt_od_flag:value(Flag).
+
+ctx_disable(From, To) ->
+    [{From, FromFlag, FromModel}] = ets:lookup(od_flag_eqc, From),
+    [{To, ToFlag, ToModel}] = ets:lookup(od_flag_eqc, To),
     Ctx = riak_dt_od_flag:precondition_context(FromFlag),
     {ok, Flag} = riak_dt_od_flag:update(disable, To, ToFlag, Ctx),
     Model = model_ctx_disable(FromModel, ToModel),
-    {To, Flag, Model}.
-
-ctx_disable_next(S=#state{replica_data=ReplicaData}, Res, _) ->
-    S#state{replica_data=[Res | ReplicaData]}.
+    ets:insert(od_flag_eqc, {To, Flag, Model}),
+    {Flag, Model}.
 
 ctx_disable_post(_S, _Args, Res) ->
     post_all(Res, ctx_disable).
 
 %% ------ Grouped operator: replicate
 %% Merge two replicas' values
-replicate_pre(S=#state{replica_data=ReplicaData}) ->
-    replicas_ready(S) andalso ReplicaData /= [].
+replicate_pre(#state{replicas=Replicas}) ->
+    Replicas /= [].
 
-replicate_args(#state{replicas=Replicas, replica_data=ReplicaData}) ->
+replicate_args(#state{replicas=Replicas}) ->
     [
      elements(Replicas), %% Replicate from
-     elements(Replicas), %% Replicate to
-     ReplicaData
+     elements(Replicas) %% Replicate to
     ].
 
-%% Don't replicate to oneself
-replicate_pre(_S, [VN, VN, _]) ->
-    false;
-replicate_pre(_S, [_VN1, _VN2, _]) ->
-    true.
+%% @doc replicate_pre - Ensure correct shrinking
+-spec replicate_pre(S :: eqc_statem:symbolic_state(),
+                    Args :: [term()]) -> boolean().
+replicate_pre(#state{replicas=Replicas}, [From, To]) ->
+    lists:member(From, Replicas) andalso lists:member(To, Replicas).
 
 %% Replicate a CRDT from `From' to `To'
-replicate(From, To, ReplicaData) ->
-    {FromFlag, FromModel} = get(From, ReplicaData),
-    {ToFlag, ToModel} = get(To, ReplicaData),
+replicate(From, To) ->
+    [{From, FromFlag, FromModel}] = ets:lookup(od_flag_eqc, From),
+    [{To, ToFlag, ToModel}] = ets:lookup(od_flag_eqc, To),
     Flag = riak_dt_od_flag:merge(FromFlag, ToFlag),
     Model = model_merge(FromModel, ToModel),
-    {To, Flag, Model}.
-
-replicate_next(S=#state{replica_data=ReplicaData}, Res, _Args) ->
-    S#state{replica_data=[Res | ReplicaData]}.
+    ets:insert(od_flag_eqc, {To, Flag, Model}),
+    {Flag, Model}.
 
 replicate_post(_S, _Args, Res) ->
     post_all(Res, rep).
 
+%% @doc weights for commands. Don't create too many replicas, but
+%% prejudice in favour of creating more than 1. Try and balance
+%% disable with enables. But favour enables so we have something to
+%% disable. See the aggregation output.
+weight(S, create_replica) when length(S#state.replicas) > 2 ->
+    1;
+weight(S, create_replica) when length(S#state.replicas) < 5 ->
+    4;
+weight(_S, disable) ->
+    3;
+weight(_S, ctx_disable) ->
+    3;
+weight(_S, enable) ->
+    6;
+weight(_S, _) ->
+    2.
 
 %% Tests the property that an od_flag is equivalent to the flag model
 prop_merge() ->
     ?FORALL(Cmds, commands(?MODULE),
             begin
-                {H, S=#state{replicas=Replicas, replica_data=ReplicaData}, Res} = run_commands(?MODULE,Cmds),
+                %% Store the state external to the statem for correct
+                %% shrinking. This is best practice.
+                ets:new(od_flag_eqc, [named_table, set]),
+                {H, S, Res} = run_commands(?MODULE,Cmds),
+                ReplicaData =               ets:tab2list(od_flag_eqc),
                 %% Check that collapsing all values leads to the same results for flag and the Model
-                {FlagValue, ModelValue} = case Replicas of
-                                             [] ->
-                                                 {[], []};
-                                             _L ->
-                                                 %% Get ALL actor's values
-                                                 {Flag, Model} = lists:foldl(fun(Actor, {F, Mo}) ->
-                                                                                    {F1, Mo1} = get(Actor, ReplicaData),
-                                                                                    {riak_dt_od_flag:merge(F, F1),
-                                                                                     model_merge(Mo, Mo1)} end,
-                                                                            {riak_dt_od_flag:new(), model_new()},
-                                                                            Replicas),
-                                                 {riak_dt_od_flag:value(Flag), model_value(Model)}
-                                         end,
+                {Flag, Model} = lists:foldl(fun({_Actor, F1, Mo1}, {F, Mo}) ->
+                                                    {riak_dt_od_flag:merge(F, F1),
+                                                     model_merge(Mo, Mo1)} end,
+                                            {riak_dt_od_flag:new(), model_new()},
+                                            ReplicaData),
+
+                {FlagValue, ModelValue} = {riak_dt_od_flag:value(Flag), model_value(Model)},
+
+                ets:delete(od_flag_eqc),
+
                 aggregate(command_names(Cmds),
                           pretty_commands(?MODULE,Cmds, {H,S,Res},
-                                          conjunction([{result,  equals(Res, ok)},
-                                                       {values, equals(FlagValue, ModelValue)}])
-                                         ))
+                                          collect(with_title(value), FlagValue,
+                                                    measure(replicas, length(ReplicaData),
+                                                            conjunction([{result,  equals(Res, ok)},
+                                                                         {values, equals(FlagValue, ModelValue)}])
+                                                           ))))
             end).
 
 %% -----------
 %% Helpers
 %% ----------
-replicas_ready(#state{replicas=Replicas, n=N}) ->
-    length(Replicas) >= N andalso N > 0.
-
-post_all({_, Flag, Model}, Cmd) ->
+post_all({Flag, Model}, Cmd) ->
     %% What matters is that both types have the exact same value.
     case riak_dt_od_flag:value(Flag) == model_value(Model) of
         true ->
@@ -256,47 +255,49 @@ post_all({_, Flag, Model}, Cmd) ->
             {postcondition_failed, "Flag and Model don't match", Cmd, Flag, Model}
     end.
 
-%% if a replica does not yet have replica data, return `new()` for the
-%% Map and Model
-get(Replica, ReplicaData) ->
-    case lists:keyfind(Replica, 1, ReplicaData) of
-        {Replica, Map, Model} ->
-            {Map, Model};
-        false -> {riak_dt_od_flag:new(), model_new()}
-    end.
-
 %% -----------
 %% Model
 %% ----------
+
+-type model() :: {Cntr :: pos_integer(), %% Unique ID per operation
+                      Enables :: list(), %% enables
+                      Disable :: list(), %% Tombstones
+                      %% Disable that are waiting for enables before they
+                      %% can be run (like context ops in the
+                      %% riak_dt_od_flag)
+                      Deferred :: list()
+                     }.
+
+-spec model_new() -> model().
 model_new() ->
-    {sets:new(), sets:new(), sets:new()}.
+    {[], [], []}.
 
 model_enable(Cnt, {Enables, Disables, Deferred}) ->
-    {ok, {sets:add_element(Cnt, Enables), Disables, Deferred}}.
+    {ok, {lists:umerge([Cnt], Enables), Disables, Deferred}}.
 
 model_disable({Enables, Disables, Deferred}) ->
-    {Enables, sets:union(Disables, Enables), Deferred}.
+    {Enables, lists:merge(Disables, Enables), Deferred}.
 
 model_merge({Enables1, Disables1, Deferred1}, {Enables2, Disables2, Deferred2}) ->
-    Enables = sets:union(Enables1, Enables2),
-    Disables = sets:union(Disables1, Disables2),
-    Deferred = sets:union(Deferred1, Deferred2),
+    Enables = lists:umerge(Enables1, Enables2),
+    Disables = lists:umerge(Disables1, Disables2),
+    Deferred = lists:umerge(Deferred1, Deferred2),
     model_apply_deferred(Enables, Disables, Deferred).
 
 model_apply_deferred(Enables, Disables, Deferred) ->
-    D2 = sets:subtract(Deferred, Enables),
-    Dis = sets:subtract(Deferred, D2),
-    {Enables, sets:union(Dis, Disables), D2}.
+    D2 = lists:subtract(Deferred, Enables),
+    Dis = lists:subtract(Deferred, D2),
+    {Enables, lists:umerge(Dis, Disables), D2}.
 
 model_ctx_disable({FromEnables, _FromDisables, _FromDeferred}, {ToEnables, ToDisables, ToDeferred}) ->
     %% Disable enables that are in both
-    Disable = sets:intersection(FromEnables, ToEnables),
+    Disable = sets:to_list(sets:intersection(sets:from_list(FromEnables), sets:from_list(ToEnables))),
     %% Defer those enables not seen in target
-    Defer = sets:subtract(FromEnables, ToEnables),
-    {ToEnables, sets:union(ToDisables, Disable), sets:union(Defer, ToDeferred)}.
+    Defer = lists:subtract(FromEnables, ToEnables),
+    {ToEnables, lists:umerge(ToDisables, Disable), lists:umerge(Defer, ToDeferred)}.
 
 model_value({Enables, Disables, _Deferred}) ->
-    Remaining = sets:subtract(Enables, Disables),
-    sets:size(Remaining) > 0.
+    Remaining = lists:subtract(Enables, Disables),
+    length(Remaining) > 0.
 
 -endif. % EQC
