6aea55f502773d8e683308ee1220248763d9e90a
balegas <v.sousa@campus.fct.unl.pt>
2015-02-10 15:45:17 +0000

    Fixed wrong merge - error found by QuickCheck

--- src/riak_dt_map.erl
+++ src/riak_dt_map.erl
@@ -468,7 +468,7 @@ propagate_remove({_, riak_dt_map}, {{Dots, _S, Tombstone}, {Clock, Value0, Defer
     case ?DICT:size(SubEntries) of
         0 ->
             {SubMergedDef, empty};
-        _ -> {SubMergedDef, {{Dots, _S, riak_dt_vclock:merge([SubMergedDef | Tombstone])}, {Clock, SubEntries, Deferred}}}
+        _ -> {SubMergedDef, {{Dots, _S, riak_dt_vclock:merge([SubMergedDef, Tombstone])}, {Clock, SubEntries, Deferred}}}
     end;
 
 %% Value is a leaf:
